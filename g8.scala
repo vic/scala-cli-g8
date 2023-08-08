dd65fe40e5d1e315c0dd0b6157d65cdcb93fdedd
Victor Borja <vborja@apache.org>
2023-08-08 11:18:20 +0000

    simplify workflow

--- g8.scala
+++ g8.scala
@@ -2,7 +2,7 @@
 //> using scala 2.12
 //> using dep org.foundweekends.giter8::giter8-launcher:0.16.2
 
-package scala_cli.giter8
+package scala_cli.g8
 
 object Main {
   def main(args: Array[String]): Unit = giter8.LauncherMain.main(args)
