802c6ea8d813314575b2cad6bbb40327c365ff9f
Victor Borja <vborja@apache.org>
2023-08-08 11:11:56 +0000

    giter8 launcher

---
+++ giter8.scala
@@ -0,0 +1,9 @@
+// -*- scala -*-
+//> using scala 2.12
+//> using dep org.foundweekends.giter8::giter8-launcher:0.16.2
+
+package scala_cli.giter8
+
+object Main {
+  def main(args: Array[String]): Unit = giter8.LauncherMain.main(args)
+}
